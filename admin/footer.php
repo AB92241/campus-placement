1941fa9ffc8a065f7d0557359b6e5b77a78f252b
AB92241 <abdulbasith9200@outlook.in>
2025-02-14 15:07:31 +0530

    first commit

---
+++ admin/footer.php
@@ -0,0 +1,44 @@
+<div class="footer">
+  <div class="container">
+    <div class="col-md-3 ftr_navi ftr">
+      <h3>NAVIGATION</h3>
+      <ul>
+        <li>
+          <a href="../index.php">Home</a>
+        </li>
+        <li>
+          <a href="../student/login.php">Student Login</a>
+        </li>
+        <li>
+          <a href="../companies/login.php">Company Login</a>
+        </li>
+
+        <li>
+          <a href="login.php">Admin Login</a>
+        </li>
+      </ul>
+    </div>
+    <!-- <div class="col-md-3 ftr_navi ftr">
+      <h3>MEMBERS</h3>
+      <ul>
+        <li>
+          <a href="#">Customer Support</a>
+        </li>
+        <li>
+          <a href="#">Placement Support</a>
+        </li>
+        <li>
+          <a href="#">Faculty Support</a>
+        </li>
+        <li>
+          <a href="#">Registered Companies</a>
+        </li>
+        <li>
+          <a href="#">Training</a>
+        </li>
+      </ul>
+    </div> -->
+   
+    </div>
+
+  </div>
